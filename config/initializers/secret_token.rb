9f6e0219f3c768bb7d72c7dd2b514089cfb886e8
James Andrew Kerr <jkerr838@gmail.com>
2014-02-19 15:21:39 -0800

    Initial Commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+EmailMachine::Application.config.secret_key_base = 'a95e73fc79cfa43038679e5d072fa1a0ee9e35a44135e1c1f558f09a51681fe85ecb754a1d43d51d93eb611d53dbe05db97a0c3413f6d8e40080a4bd9e8a52f1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+EmailMachine::Application.config.session_store :cookie_store, key: '_EmailMachine_session'
